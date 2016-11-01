6d50ff10a3af6e0cc06722dae2bd5058c23cd295
Jeonghyun Kim <kimjh@bawi.org>
2016-11-02 00:15:48 +0900

    delete free

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -118,7 +118,7 @@ timer_wakeup (void)
 {
   struct list_elem *e;
 
-  for (e = list_begin (&sleep_list); e != list_end (&sleep_list);) {
+  for (e = list_begin (&sleep_list); e != list_end (&sleep_list); e = list_next (e)) {
     struct sleep_info *l = list_entry (e, struct sleep_info, elem);
 
     if (l->time_remain > 0)
@@ -129,7 +129,6 @@ timer_wakeup (void)
     if (l->time_remain == 0) {
       thread_unblock (l->thread_elem);
       list_remove (e);
-      free(l);
     }
   }
 }
