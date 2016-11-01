be6330ad6ee64ce52cbbddf4c653484a30cffa68
Jeonghyun Kim <kimjh@bawi.org>
2016-11-01 22:44:39 +0900

    initial commit

---
+++ pintos/src/tests/threads/print-name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "tests/threads/tests.h"
+
+void test_print_name() {
+  msg ("\r*****************************\n"
+	"2014193009	\n"
+	"Jeonghyun Kim\n"
+	"Yonsei Univ.\n"
+	"School of Integrated Tech.\n"
+	"Computer Operating Systems\n"
+	"*****************************");
+}
+
