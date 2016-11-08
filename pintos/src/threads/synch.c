0cdc83ef02b14df4229eccc69999fe07a8d325ca
Jeonghyun Kim <kimjh@bawi.org>
2016-11-09 00:00:40 +0900

    add &

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -207,7 +207,7 @@ lock_acquire (struct lock *lock)
       lock->original_priority = thread_get_priority ();
       struct lock_priority *p;
       p->donation_priority = thread_get_priority ();
-      list_push_front (&lock->holder->donation_priorities, p->elem);
+      list_push_front (&lock->holder->donation_priorities, &p->elem);
       lock->holder->priority = thread_get_priority ();
       thread_yield ();
     }
