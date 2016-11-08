cf4ccbe621fa8a33ad5645f2c68a0cee328a4926
Jeonghyun Kim <kimjh@bawi.org>
2016-11-08 19:05:23 +0900

    Revert "debugasdf"
    This reverts commit 8066db01e9ea5e808072ebdd1297df418c8fb2ca.

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -247,10 +247,10 @@ lock_release (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
   if (lock->original_priority) {
-    // if (lock->donater == thread_current ()) {
+    if (lock->donater == thread_current ()) {
       lock->holder->priority = lock->original_priority;
       lock->original_priority = NULL;
-    // }
+    }
   }
   lock->holder = NULL;
   sema_up (&lock->semaphore);
