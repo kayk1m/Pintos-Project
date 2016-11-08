542e458e0918cfa4218621ed87aef6faee2524e3
Jeonghyun Kim <kimjh@bawi.org>
2016-11-08 18:59:46 +0900

    Revert "debug"
    This reverts commit e72b92df54a06641c4837957a91f77be576c89b3.

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -209,9 +209,6 @@ lock_acquire (struct lock *lock)
       lock->donater = thread_current ()->tid;
       thread_yield ();
     }
-    else {
-      lock->original_priority = lock->holder->priority;
-    }
   }
   sema_down (&lock->semaphore);
   lock->holder = thread_current ();
