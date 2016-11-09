df2d060e4d36c5bcdc21e00242c133593060438e
Byeongjoon <bjkimp52014@gmail.com>
2016-11-09 20:40:42 +0900

    lalalalala

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -474,7 +474,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
   list_push_back (&all_list, &t->allelem);
-  t->original_priority = NULL;
+  t->original_priority = -1;
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
