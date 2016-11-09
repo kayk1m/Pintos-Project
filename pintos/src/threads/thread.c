72652b64255b31f1bd85552456f0fcdcd5c6b948
Byeongjoon <bjkimp52014@gmail.com>
2016-11-09 20:39:13 +0900

    i can't sure that it can fix error

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -474,6 +474,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
   list_push_back (&all_list, &t->allelem);
+  t->original_priority = NULL;
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
